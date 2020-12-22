ea14c968a2cb7c47745908cf041652b4d58ae085
Leonid Romanovsky <sirleonidromanovsky@gmail.com>
2020-12-22 10:49:28 +0400

    fixed critical bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const AWS = require('aws-sdk')
 const AWSMqttmqttclient = require('aws-mqtt/lib/NodeClient')
-const SerialPort = require('serialport')
+const dgram = require('dgram')
 const path = require('path')
 const { throttle } = require('lodash')
 const { MAVLink20Processor, mavlink20 } = require('./MAVLink20')
@@ -27,38 +27,36 @@ const mqttclient = new AWSMqttmqttclient({
   }
 })
 
-const serialport = new SerialPort(
-  config.serial.path,
-  { baudRate: config.serial.baudRate },
-  exitOnError
-)
+const udp_socket = dgram.createSocket('udp4')
+let udp_socket_isOpen = false
+udp_socket.bind(config.udp.server || {}, () => udp_socket_isOpen = true)
 
 const mav2 = new MAVLink20Processor()
 
 // Connect all together
 
 mqttclient.on('connect', () => {
-  mqttclient.subscribe(config.topicToThing, exitOnError)
+  mqttclient.subscribe(config.topicFromThing, exitOnError)
 })
 
 mqttclient.on('message', (topic, buff) => {
-  if (serialport.isOpen) {
+  if (udp_socket_isOpen) {
     console.log('recv', buff.length)
-    serialport.write(buff)
+    udp_socket.send(buff, config.udp.gcs.port, config.udp.gcs.address)
   }
   else
     console.log('skip', 'recv', buff.length)
 })
 
-serialport.on('data', buff => {
+udp_socket.on('message', buff => {
   for (const message of mav2.parseBuffer(buff)) {
     if (message instanceof mavlink20.messages.bad_data) {
-      pong()
+      console.log('skip', 'send', message.msgbuf.length, 'as', message.name)
     }
     else {
       if (mqttclient.connected) {
         console.log('send', message.msgbuf.length, 'as', message.name)
-        mqttclient.publish(config.topicFromThing, message.msgbuf)
+        mqttclient.publish(config.topicToThing, message.msgbuf)
       }
       else {
         console.log('skip', 'send', message.msgbuf.length, 'as', message.name)
@@ -67,18 +65,21 @@ serialport.on('data', buff => {
   }
 })
 
-serialport.on('error', exitOnError)
-serialport.on('close', () => exitOnError(new Error('Serialport closed')))
+udp_socket.on('error', exitOnError)
+udp_socket.on('close', () => {
+  udp_socket_isOpen = false
+  exitOnError(new Error('UPD socket closed'))
+})
+mqttclient.on('error', exitOnError)
 mqttclient.on('close', () => exitOnError(new Error('MQTTClient closed')))
 mqttclient.on('disconnect', () => exitOnError(new Error('MQTTClient disconnected')))
 mqttclient.on('offline', () => exitOnError(new Error('MQTTClient went offline')))
 
 // Cleanup on exit
 
-
 process.on('exit', () => {
   mqttclient.end(true)
-  serialport.close()
+  udp_socket.close()
 })
 
 // Utils
